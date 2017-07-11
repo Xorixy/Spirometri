59f270ef26bde93eb046981351bf5d288f292407
Xorixy <soricib@gmail.com>
2017-07-11 11:30:20 +0200

    Update VarvPerSekund.py

--- VarvPerSekund.py
+++ VarvPerSekund.py
@@ -1,26 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Jun 30 16:41:16 2017
-
-@author: Neo
-"""
-
-# -*- coding: utf-8 -*-
-#384, 512, 3
-#Linn (Snurr3) 1 187 skip
-#Linn (Snurr6) 243
-"""
-Created on Mon Jun 26 13:36:31 2017
-
-@author: Neo
-"""
-skip = 0
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
 #from scipy import stats
 
-
+#Takes an array containing frequency lists and makes a graph from each frequency list
 def printaFlera(manylists):
     for p in range (len(manylists)):
         for q in range (len(manylists[p]) - 1):
@@ -36,36 +19,36 @@ def printaFlera(manylists):
     plt.savefig('Philip.svg')
     plt.show
 
-def plotta(x, y):   
-    plt.scatter(x,y)
-    plt.plot(x, y)
-    plt.show()
     
-
+#Analyses a film and returns a list containing the mean light intensity of each frame
 def countPixlar(color_array, cap, frames, skip):
     for p in range (frames-1):
-        #Läser in en bild
+        #Reads a frame
         ret, frame = cap.read()
-        #Skippar upp till skip-värdet
+        #Skips to the skip value
         if(p < skip):
             continue
         snurr = frame
-        #Avgrämsar till den relevanta delen
+        #Cuts the movie into the relevant parts
         snurr = frame[92:641, 247:997]
-        #snurr = frame[76:598, 233:662]
-        #Omvandlar bilden till gråskala
+        #Turns the picture into grayscale
         gray = cv2.cvtColor(snurr, cv2.COLOR_BGR2GRAY)
-        #Tar medelvärdet av alla pixlarna
+        #Calculates the mean value of the frame
         average_color_per_row = np.average(gray, axis=0)
         average_color = np.average(average_color_per_row, axis=0)
         color_array[p - skip] = average_color
     return color_array;
 
-def frequency(freqlist, color_array, medel):
-    c = 0 #räknare, räknar upptill 24 
-    k = 0 #räknare, räknar antalet extrempunkter/vändningar
-    p = 0  #räknare som loopar genom listan
+#Calculates how many times the light intensity crossed the mean value of the whole film
+# for every 48 frames, then returns that in a list
+def frequency(freqlist, color_array):
+    #Calculates the mean value of the whole film
+    medel = np.mean(color_array)
+    c = 0 #Counter, counts up to 47
+    k = 0 #Counter, counts the total times that the light intensity crossed the mean value
+    p = 0 #Counter, loops through the whole film
     while p < len(color_array) :
+        #Loops until the light intensity in the frame goes under the mean
         if color_array[p] > medel:
             while p < len(color_array) and color_array[p]> medel:
                 if c == 47:
@@ -76,7 +59,8 @@ def frequency(freqlist, color_array, medel):
                 p = p+1
                 c= c+1 
             k += 1
-    
+        
+        #Loops until the light intensity in the frame goes over the mean
         else:
             while p < len(color_array) and color_array[p] <= medel: 
                 if c == 47:
@@ -89,27 +73,28 @@ def frequency(freqlist, color_array, medel):
             k = k +1 
     return freqlist;
 
+#Takes a movie and returns a frequency list containing the times the light intensity crossed the mean value per 48 frames
 def getFreqList(name, skip):
+    #Reads the film
     cap = cv2.VideoCapture(name)
-
+    
+    #Counts the number of frames
     frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
     color_array = np.zeros(frames - skip)
-    medel = 0
 
 
-    #Skapar en lista med medelvärdena av pixlarnas ljusintensitet per bild
+    #Creates a list of the mean light intensity values per frame
     
     color_array = countPixlar(color_array, cap, frames, skip)
-    
-    #Medelvärdet av pixlarna genom hela filmen
-
-    medel = np.mean(color_array)
-    
+        
     freqlist = []
-
-    freqlist = frequency(freqlist, color_array, medel)
+    
+    #Creates a list of the times the light intensity crossed the mean
+    freqlist = frequency(freqlist, color_array)
     return freqlist
     
+
+#Makes a graph from a frequency list
 def printaLista(freqlist):
     for q in range (len(freqlist) - 1):
         freqlist[q] *= 2.5
@@ -123,12 +108,13 @@ def printaLista(freqlist):
 
 
         
-
+#Exports a frequency list to a txt
 def exportLista(thelist, name):
     thefile = open(name, 'w')
     for item in thelist:
         thefile.write("%s\n" % item)
 
+#Imports a frequency list from a txt
 def readLista(name):
     thefile = open(name, 'r')
     lines = thefile.read().split('\n')
@@ -139,7 +125,7 @@ def readLista(name):
     return lines2
 
 
-
+#Gets the max value of a list
 def getMax(freqlist):
     largestValue = 0
     for number in freqlist:
@@ -147,6 +133,7 @@ def getMax(freqlist):
             largestValue = number;
     return largestValue
 
+#Gets a list containing the mean light intensity for each frame of a video
 def getCArray(name, skip):
     cap = cv2.VideoCapture(name)
 
@@ -159,12 +146,9 @@ def getCArray(name, skip):
     color_array = countPixlar(color_array, cap, frames, skip)
     #Medelvärdet av pixlarna genom hela filmen
     return color_array
-#def printMaxes()
 
 
-#Hur många bilder som ska skippas i början
-skip = 0
-#lista = getFreqList(name, skip)
+
 
 flerlist = []
 
